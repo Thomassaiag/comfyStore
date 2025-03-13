d842e75d04edc7ea659d421c32e32ec5aa173555
Thomassaiag <thomas.saiag@gmail.com>
2025-03-13 11:35:32 +0100

    created about page

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+				perferendis pariatur vitae facilis quis ratione, unde dolorem ab
+				placeat eius fugit totam! Doloribus enim voluptatem
+				reprehenderit consectetur quod, incidunt fugit!
+			</p>
+		</>
+	);
+};
+
+export default About;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <h1 className='text-4xl'>About</h1>
-  )
-}
+	return (
+		<>
+			<div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+				<h1 className="text-4xl font-bold leading-none tracking-tight sm: text-6xl">
+					We love
+				</h1>
+				<div className="stats shadow bg-primary">
+					<div className="stat">
+						<div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+							Comfy
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Accusamus neque id non. Labore dolores enim commodi tempora
+				repudiandae iusto itaque aliquid quibusdam pariatur voluptate
+				quia provident repellendus unde, dolorem quisquam.
+			</p>
+		</>
+	);
+};
 
-export default About
+export default About;
